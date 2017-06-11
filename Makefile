d07f7f846151b053b6816e1249092c97bc07a381
alexandre thauvin <alexandre.thauvin@epitech.eu>
2017-06-11 15:06:49 +0200

    norme 1

--- Makefile
+++ Makefile
@@ -10,19 +10,7 @@
 
 SERVER	= server
 
-CLIENT	= client
-
-DIR1    = src/client/
-
 DIR2    = src/server/
-
-#RULE	= 2
-
-SRC1    = $(DIR1)main.c \
-          $(DIR1)fill_buff.c \
-	  $(DIR1)pars_func.c \
-	  $(DIR1)msg_command.c \
-	  $(DIR1)join_command.c
 
 SRC2    = $(DIR2)main.c \
 	$(DIR2)tools.c \
@@ -36,17 +24,13 @@ SRC2    = $(DIR2)main.c \
 	$(DIR2)cond.c \
 	$(DIR2)propagation.c
 
-OBJ1	= $(SRC1:.c=.o)
-
 OBJ2	= $(SRC2:.c=.o)
 
 GCC	= gcc
 
 RM	= rm -rf
 
-CFLAGS	=  -Wall -W -Wextra -g
-
-INC1    = -I ./includes/client/
+CFLAGS	=  -Wall -W -Wextra
 
 INC2    = -I ./includes/server/
 
@@ -54,22 +38,17 @@ INC2    = -I ./includes/server/
 $(SERVER): $(OBJ2)
 	$(GCC) $(OBJ2) -o $(SERVER) $(CFLAGS) $(INC2)
 
-$(CLIENT): $(OBJ1)
-	$(GCC) $(OBJ1) -o $(CLIENT) $(CFLAGS) $(INC1)
-
-all: $(SERVER) $(CLIENT)
+all: $(SERVER)
 
 .c.o:
-	$(GCC) $(CFLAGS) $(INC1) $(INC2) -c $< -o $@
+	$(GCC) $(CFLAGS) $(INC2) -c $< -o $@
 
 clean:
-	$(RM) $(OBJ1)
 	$(RM) $(OBJ2)
 	$(RM) *~
 	$(RM) *#
 
 fclean: clean
 	$(RM) $(SERVER)
-	$(RM) vgcore*
 
 re: fclean all
--- src/server/default.c
+++ src/server/default.c
@@ -5,7 +5,7 @@
 ** Login   <thauvi_a@epitech.net>
 **
 ** Started on  Fri Jun  9 22:08:55 2017 Alexandre Thauvin
-** Last update Fri Jun  9 22:08:57 2017 Alexandre Thauvin
+** Last update Sun Jun 11 14:58:04 2017 Alexandre Thauvin
 */
 
 #include "server.h"
@@ -15,35 +15,27 @@ void	choice(t_serv *serv, int fd)
   void		(*func[12])(t_client *, t_serv *) = {f_nick, f_user, f_list, f_join, f_part,
 				       f_users, f_names, f_msg,  f_send_file, f_accept_file, f_quit, f_cap};
   void		(*funci)(t_client *, t_serv *);
-  int		i;
   t_client	*tmp;
-  int 		u = 0;
-  static int 	check = 0;
+  int 		u, i;
 
-  i = 0;
+  i = u = 0;
   tmp = find_clt(serv->head, fd, serv);
   while (tmp->cmd[u] != NULL)
     u++;
   while (serv->tab[i] != NULL && strcmp(tmp->cmd[0], serv->tab[i]) != 0)
     i++;
-//  printf("[default]cmd: %s\n", tmp->cmd[0]);
-  if (i <= 10)
+  if (i <= 11)
   {
     funci = func[i];
     (*funci)(tmp, serv);
   }
   else
-  {
-    if (check == 1)
-      print_at_all(tmp, serv);
-    check = 1;
-  }
+    print_at_all(tmp, serv);
   if (tmp->registered == 0 && strcmp(tmp->nickname, "anonymous") != 0 && tmp->user != false)
   {
     dprintf(tmp->fd, "001 %s :Welcome\r\n", tmp->cmd[1]);
     tmp->registered = 1;
   }
-
 }
 
 bool 	check_end(t_client *clt)
